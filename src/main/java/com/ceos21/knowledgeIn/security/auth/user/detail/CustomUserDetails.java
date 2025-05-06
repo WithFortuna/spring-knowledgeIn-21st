b8b1823873a712932864e0486700bcbf79ce63b4
WithFortuna <rmsghchl0@naver.com>
2025-05-06 15:49:40 +0900

    refactor: UserDetail 사용자정보 관리방식 변경
    기존: 엔티티 노출
    변경: DTO

--- src/main/java/com/ceos21/knowledgeIn/security/auth/user/detail/CustomUserDetails.java
+++ src/main/java/com/ceos21/knowledgeIn/security/auth/user/detail/CustomUserDetails.java
@@ -1,27 +1,50 @@
 package com.ceos21.knowledgeIn.security.auth.user.detail;
 
+import com.ceos21.knowledgeIn.domain.user.Role;
 import com.ceos21.knowledgeIn.domain.user.User;
+import jakarta.persistence.Column;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
-@RequiredArgsConstructor
+@Builder
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
 public class CustomUserDetails implements UserDetails {
-    private final User user;
+    private Long userId;
+    private String name;
+    private String nickname;
+    private String username;
+    private String password;
+    private List<Role> roles;
+
+    static public CustomUserDetails from(User entity) {
+        return CustomUserDetails.builder()
+                .userId(entity.getId())
+                .name(entity.getName())
+                .nickname(entity.getNickname())
+                .username(entity.getUsername())
+                .password(entity.getPassword())
+                .roles(new ArrayList<>(entity.getRoles()))
+                .build();
+    }
 
     public Long getUserId() {
-        return user.getId();
+        return userId;
     }
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        List<SimpleGrantedAuthority> authorities = user.getRoles().stream()
+        List<SimpleGrantedAuthority> authorities = roles.stream()
                 .map(role -> new SimpleGrantedAuthority(role.name()))
                 .collect(Collectors.toList());
         return authorities; // 필요시 ROLE_USER 등 추가 가능
@@ -29,12 +52,12 @@ public class CustomUserDetails implements UserDetails {
 
     @Override
     public String getPassword() {
-        return user.getPassword();
+        return password;
     }
 
     @Override
     public String getUsername() {
-        return user.getUsername();
+        return username;
     }
 
     @Override
--- src/main/java/com/ceos21/knowledgeIn/security/auth/user/detail/CustomUserDetailsService.java
+++ src/main/java/com/ceos21/knowledgeIn/security/auth/user/detail/CustomUserDetailsService.java
@@ -22,6 +22,6 @@ public class CustomUserDetailsService implements UserDetailsService {
         User user = userRepository.findByUsername(username)
                 .orElseThrow(() -> new UsernameNotFoundException("사용자를 찾을 수 없습니다."));
 
-        return new CustomUserDetails(user);
+        return CustomUserDetails.from(user);
     }
 }
